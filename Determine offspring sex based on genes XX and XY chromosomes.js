14c170bd490c63afba0bc3b17fc68e85de359cb8
jazzfelton1911 <59237581+jazzfelton1911@users.noreply.github.com>
2023-03-17 11:07:04 -0400

    Update Determine offspring sex based on genes XX and XY chromosomes.js

--- Determine offspring sex based on genes XX and XY chromosomes.js
+++ Determine offspring sex based on genes XX and XY chromosomes.js
@@ -9,3 +9,6 @@
 //based on the X or Y chromosome present in the male's sperm. If the sperm contains the X chromosome, 
 //return "Congratulations! You're going to have a daughter."; If the sperm contains the Y chromosome, return "Congratulations! You're going to have a son.";
 
+function chromosomeCheck(sperm) {
+  return `Congratulations! You're going to have a ${sperm === 'XY' ? 'son' : 'daughter'}.`
+}
