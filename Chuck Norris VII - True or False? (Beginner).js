2f3d3023265cdd581128cb205f7d422978be8c6e
DevJazzFelton <128212075+DevJazzFelton@users.noreply.github.com>
2023-03-18 03:15:48 -0400

    Add files via upload

---
+++ Chuck Norris VII - True or False? (Beginner).js
@@ -0,0 +1,12 @@
+// It's a well known fact that anything Chuck Norris wants, he gets. As a result Chuck very rarely has to use the word false.
+// It is a less well known fact that if something is true, and Chuck doesn't want it to be, Chuck can scare the truth with his massive biceps, and it automatically becomes false.
+// Your task is to be more like Chuck (ha! good luck!). You must return false without ever actually using the word false...
+// Go show some truth who's boss!
+
+function ifChuckSaysSo() {
+    return !1;
+  }
+
+//This JavaScript function is called "ifChuckSaysSo". It uses the "return" keyword to return the boolean value "false" by negating the number "1" using the logical NOT operator (!).
+// In JavaScript, the number 1 is considered a "truthy" value, meaning that when used in a boolean context, it is evaluated as true. By negating it using the logical NOT operator, it becomes a "falsy" value, or false when evaluated in a boolean context.
+// Therefore, the function "ifChuckSaysSo" always returns false. The name of the function suggests that it is related to the famous actor and martial artist Chuck Norris, who is often the subject of jokes and memes online.
